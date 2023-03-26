462e22432bba32f88e913d9beb9d4fcc03633d5f
mykaellyoliveira <mykaelly.odc02@gmail.com>
2023-03-26 00:48:03 -0300

    form de registro de item

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,25 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter'
+const fileInput = document.querySelector<HTMLInputElement>("input[type=file]")!
+const img = document.createElement("img")
+const output = document.querySelector<HTMLDivElement>(".output")!
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+fileInput.addEventListener("change", () => {
+  const file = fileInput.files;
+  const reader = new FileReader();
+  if (file) {
+    reader.readAsDataURL(file[0])
+  }
+  reader.addEventListener("load", () => {
+    const url: string = reader.result as string
+    img.setAttribute("SRC", url)
+    img.style.width = "120px"
+    img.style.height = "120px"
+    
+    
+    output.appendChild(img);
+  });
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+});
+
+
+
+export { };
