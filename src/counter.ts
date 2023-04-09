70bcf8267675e7ded34f14688e8a247022b8e320
mykaellyoliveira <mykaelly.odc02@gmail.com>
2023-04-09 20:21:36 -0300

    página de cadastro e listagem de itens pronta

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,48 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+// pegando itens do json e adicionando nas divs 
+export interface Itens{
+  name: string;
+  category: string;
+  price: number;
+  image: string;
 }
+
+const route = 'http://localhost:3000/itens'
+
+const itens = fetch(route).then(res => res.json())
+
+console.log(itens)
+
+const main = document.querySelector('main');
+
+itens.then((response: Itens[]) => {
+   response.forEach((item) => {
+    const card = document.createElement('itens-card')
+    card.addSlot('name', item.name)
+    card.addSlot('category', item.category)
+    card.addSlot('price', item.price)
+    card.addSlot('image', item.image)
+    main?.appendChild(card)
+
+  })
+})
+
+const itemTemplate = document.querySelector<HTMLTemplateElement>('#item')
+
+class ItemCardElement extends HTMLElement{
+  connectedCallback(){
+    if(itemTemplate){
+      const fragment = itemTemplate.content.cloneNode(true) as DocumentFragment
+      const shadow = this.attachShadow({mode: 'open'})
+      shadow.appendChild(fragment)
+    }
+  }
+  
+  addSlot(name: 'name' | 'category' | 'price' | 'image', value: string){
+    const span = document.createElement('span')
+    span.slot = name
+    span.innerText = value
+    this.appendChild(span)
+  }
+}
+
+customElements.define('itens-card', ItemCardElement)
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 const fileInput = document.querySelector<HTMLInputElement>("input[type=file]")!
 const img = document.createElement("img")
 const output = document.querySelector<HTMLDivElement>(".output")!
+const inputname = document.querySelector<HTMLInputElement>("#name")!
+const inputcategory = document.querySelector<HTMLSelectElement>("#category")!
+const inputprice = document.querySelector<HTMLInputElement>("#price")!
+const button = document.querySelector<HTMLButtonElement>("#add")!
 
+let url: string = ""
+let jsonString = ""
+//carregando imagem na div 
 fileInput.addEventListener("change", () => {
   const file = fileInput.files;
   const reader = new FileReader();
@@ -9,17 +16,41 @@ fileInput.addEventListener("change", () => {
     reader.readAsDataURL(file[0])
   }
   reader.addEventListener("load", () => {
-    const url: string = reader.result as string
+    url = reader.result as string
     img.setAttribute("SRC", url)
     img.style.width = "120px"
     img.style.height = "120px"
-    
-    
+
     output.appendChild(img);
   });
-
 });
 
+// pegando informações do formulário transformando em json e jogando na rota
+button.addEventListener("click", async (event) => {
+  event.preventDefault();
+ 
+    const item  = {
+      name: inputname.value, 
+      category: inputcategory.options[inputcategory.selectedIndex].text,
+      price: parseFloat(inputprice.value),
+      image: url,
+    }
+
+    const response = await fetch('http://localhost:3000/itens', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(item)
+    })
+
+    inputname.value = '';
+    inputprice.value = '';
+    fileInput.value = '';
+    output.removeChild(img);
+
+    alert("Item adicionado a lista com sucesso")
+})
 
 
-export { };
+
